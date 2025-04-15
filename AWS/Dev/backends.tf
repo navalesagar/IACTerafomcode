49ce86564c7ed17373282b27f2b14f35f3c3b112
navalesagar <navalesagar9143@gmail.com>
2025-04-15 23:26:00 +0530

    added some commands

---
+++ AWS/Dev/backends.tf
@@ -0,0 +1,10 @@
+terraform { 
+  cloud { 
+    
+    organization = "sjinfotech" 
+
+    workspaces { 
+      name = "IACcodeworkspace" 
+    } 
+  } 
+}
---
+++ AWS/terraform.sh
@@ -0,0 +1,31 @@
+
+#terraform log in enter api tokens from terrafrom cloud  
+
+
+terraform login
+
+
+
+#inilize 
+terrafrom init
+# show terraform tree stucture 
+tree .terraform
+
+#show all directory files such as hidden file
+ls -altr 
+
+# validate configuration files for syntax
+terraform validate
+
+#create an execution plan by scanning all *.tf files in current directory. 
+terraform plan
+
+#IAC code excute and apply providers create resourse actully 
+terraform apply 
+
+# delete all create infrastucture
+terraform destroy
+# amazon install teraform ami 
+sudo yum install -y yum-utils shadow-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
