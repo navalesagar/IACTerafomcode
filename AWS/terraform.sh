acf094275be50d4cc9bb01ffab94f346a25ae3f1
navalesagar <165131807+navalesagar@users.noreply.github.com>
2025-04-15 12:53:57 +0530

    Update terraform.sh

---
+++ AWS/terraform.sh
@@ -0,0 +1,17 @@
+#inilize 
+terrafrom init
+# show terraform tree stucture 
+tree .terraform
+
+#show all directory files such as hidden file
+ls -altr 
+
+#create an execution plan by scanning all *.tf files in current directory. 
+terraform plan
+
+#IAC code excute and apply providers create resourse actully 
+terraform apply 
+
+# delete all create infrastucture
+terraform destroy
+
