84915aaa0c15e4027fcf7c05050fbefefb515b8a
navalesagar <navalesagar9143@gmail.com>
2025-04-20 01:15:30 +0530

    adde . terrafrom file check gitignore skip the file

---
+++ AWS/QA/backend.tf
@@ -0,0 +1,9 @@
+
+#create bucket for backup store bucket=name , key=folder with file name 
+/*terraform {
+ backend "s3" {
+bucket="terraform9143" 
+key="terraformstate/backup"
+region="us-east-1"   
+ }
+}*/
